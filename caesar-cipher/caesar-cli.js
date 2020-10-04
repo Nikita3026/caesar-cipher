b8541026f86a4d068322c0348ba8f2118cfcad41
Nikita Mihadyuk <mihadyuk.nikita@gmail.com>
2020-10-04 21:08:53 +0300

    feat:added full solution to the task

---
+++ caesar-cipher/caesar-cli.js
@@ -0,0 +1,17 @@
+const {program} = require('commander');
+const createPipeline = require('./create-pipeline');
+const paramsErrorCheck = require('./error-check');
+
+program
+.requiredOption('-s, --shift <shift>', 'Cipher shift value')
+.option('-i, --input <filePath>', 'File path for input value')
+.option('-o, --output <filePath>', 'File path for output value')
+.requiredOption('-a, --cli-action <encode/decode>', 'Encode or decode cipher')
+.parse(process.argv);
+
+try{
+    paramsErrorCheck(program.opts().shift, program.opts().cliAction)
+    createPipeline(program.opts());
+}catch(error) {
+    console.log(error.message);
+}
---
+++ caesar-cipher/create-pipeline.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const {pipeline} = require('stream');
+const through2 = require('through2');
+const dataEncoding = require('./dataEncoding');
+
+module.exports = (args) => {
+    return pipeline(
+        args.input? fs.createReadStream(args.input, 'utf8') : process.stdin,
+        through2(function(chunk, _, callback){
+            this.push(dataEncoding(chunk, args.shift, args.cliAction))
+            callback()
+        }),
+        args.output ? fs.createWriteStream(args.output) : process.stdout,
+        (err) => {
+            if(err) console.error('Error! Unable to read or write to file. Please, check file paths.');
+            else console.log('Record succeeded.');
+        }
+    )
+};
